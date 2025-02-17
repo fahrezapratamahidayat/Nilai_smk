02d236b16e6ae6a7f0bdaf87e157ccd7a25ea654
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-02-17 20:02:41 +0700

    f

---
+++ database/migrations/2025_02_17_030149_create_wali_kelas_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wali_kelas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // Nama Wali Kelas
+            $table->string('email')->unique(); // Email untuk login
+            $table->string('password'); // Password untuk login
+            $table->enum('kelas_ajar', ['10', '11', '12']); // Kelas yang diajar
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wali_kelas');
+    }
+};
